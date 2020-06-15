3850e0371e5252d6852a185e494f08d666b3119f
Fredrik Ståhl <fredrik.stahl@fluidogroup.com>
2020-06-15 15:01:34 +0200

    Updates

---
+++ precache-manifest.3ffbcb1117797dc1f030eaee53aa1234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a96ad474284a7c9dcc344707da33a6",
+    "url": "/med-andra-ord/index.html"
+  },
+  {
+    "revision": "515521fbf3e297e85963",
+    "url": "/med-andra-ord/static/css/main.e2feca4d.chunk.css"
+  },
+  {
+    "revision": "6e00d7278a968d0f9b67",
+    "url": "/med-andra-ord/static/js/2.f3b08f82.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/med-andra-ord/static/js/2.f3b08f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "515521fbf3e297e85963",
+    "url": "/med-andra-ord/static/js/main.9bbe59bf.chunk.js"
+  },
+  {
+    "revision": "7f3f01bd468ce18d1fdd",
+    "url": "/med-andra-ord/static/js/runtime-main.0509344b.js"
+  }
+]);
