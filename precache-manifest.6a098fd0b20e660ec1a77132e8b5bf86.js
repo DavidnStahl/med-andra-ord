72439521cfc61c1122299657cbe58b2caa51465b
Fredrik Ståhl <fredrik.stahl@fluidogroup.com>
2020-06-15 15:12:33 +0200

    Updates

---
+++ precache-manifest.6a098fd0b20e660ec1a77132e8b5bf86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64279a2b2a9f2eb32f7de509f56acdf",
+    "url": "/medandraord/index.html"
+  },
+  {
+    "revision": "b49502ec704cf74ca515",
+    "url": "/medandraord/static/css/main.e2feca4d.chunk.css"
+  },
+  {
+    "revision": "6e00d7278a968d0f9b67",
+    "url": "/medandraord/static/js/2.f3b08f82.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/medandraord/static/js/2.f3b08f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49502ec704cf74ca515",
+    "url": "/medandraord/static/js/main.189113d0.chunk.js"
+  },
+  {
+    "revision": "2c1b23fe37237149985c",
+    "url": "/medandraord/static/js/runtime-main.b02c969e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/med-andra-ord/precache-manifest.3ffbcb1117797dc1f030eaee53aa1234.js"
+  "/medandraord/precache-manifest.6a098fd0b20e660ec1a77132e8b5bf86.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/med-andra-ord/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/medandraord/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
